f1a456ac23aef6f824bf26d53195a74dc729e041
Junhyeok17 <80610714+Junhyeok17@users.noreply.github.com>
2024-03-20 16:08:17 +0900

    Add files via upload
    first commit

---
+++ AftercareWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.IO;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+//using System.Windows.Controls;
+using System.Threading;
+using Org.BouncyCastle.Asn1.Crmf;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace AntivirusApp
+{
+    public partial class AftercareWindow : Window
+    {
+        public AftercareWindow()
+        {
+            InitializeComponent();
+            caredFiles.Dispatcher.Invoke(() =>
+            {
+                caredFiles.Text = "삭제한 악성 파일 수: " + ResultWindow.maliciousFiles;
+            });
+        }
+
+        private void GGoHomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            // 홈으로 돌아가는 버튼 클릭 시 홈 창 열기
+            MainWindow homeWindow = new MainWindow();
+            homeWindow.Show();
+            this.Close();
+        }
+    }
+}
